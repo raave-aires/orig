631bf9cb7a2ac1a9c5f9311f34b7bb1a4cd3a6de
Raavë L. Aires <raave.aires@proton.me>
2024-09-26 12:48:41 +0000

    placeholder pror form misto

--- app/components/Form/FormMisto/FormMisto.tsx
+++ app/components/Form/FormMisto/FormMisto.tsx
@@ -3,11 +3,14 @@ import { format, subDays } from "date-fns";
 
 export function FormMisto(){
     const hoje = new Date();
-    const fData = format(subDays(hoje, 3), "MM-dd-yyyy");
+    const fData = format(subDays(hoje, 3), "12.0yy-MM-dd");
 
     return(
         <>
-            {`${fData}`}
+            <div className="py-5">
+                <p>está parte está em desenvolvimento.</p>
+                <p>&#169; {`${fData}`} raaviüs.</p>
+            </div>
         </>
     );
 }
