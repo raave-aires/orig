1e336a2847fce04c581eccc632b97563eb5c828f
raave-aires <raave.aires@proton.me>
2024-10-22 13:21:47 -0300

    content do tailwind corrigido

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,8 @@ import {nextui} from "@nextui-org/react";
 
 const config: Config = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/conta/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
         "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
